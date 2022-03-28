8acec4b5906950b46cc88cc0a037f191f12a0237
Zane Birkett <axelbirkett@gmail.com>
2022-03-28 13:06:54 -0400

    Objects & Arrays

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,37 @@
-let character = "mario";
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ["luigi", "mario", "yoshi"];
 
-character = "luigi";
-age = 40;
-isBlackBelt = true;
+names.push("toad");
+// names.push(3);
+// names[1] = 3;
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+let numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed = ["ken", 4, "chun-li", 8, 9];
+
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30,
 };
 
-console.log(circ(30));
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: "yoshi",
+  belt: "orange",
+  age: 40,
+  // skills: ['running'],
+};
